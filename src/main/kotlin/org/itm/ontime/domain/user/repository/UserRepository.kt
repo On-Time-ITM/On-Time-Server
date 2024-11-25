424df84f875332f1552985dba901bcaa92234131
saeyeonn <sy01lim@naver.com>
2024-11-25 15:48:53 +0900

    :bug: Fix: UserRepository에서 findAllById가 쿼리 에러 수정

--- src/main/kotlin/org/itm/ontime/domain/user/repository/UserRepository.kt
+++ src/main/kotlin/org/itm/ontime/domain/user/repository/UserRepository.kt
@@ -2,10 +2,12 @@ package org.itm.ontime.domain.user.repository
 
 import org.itm.ontime.domain.user.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import java.util.*
 
 interface UserRepository : JpaRepository<User, UUID> {
     fun existsByPhoneNumber(phoneNumber: String): Boolean
     fun findByPhoneNumber(phoneNumber: String): User?
-    fun findAllById(map: List<UUID>): MutableList<User>
+    @Query("SELECT u FROM User u WHERE u.id IN :ids")
+    fun findAllById(ids: MutableList<UUID>): MutableList<User>
 }
