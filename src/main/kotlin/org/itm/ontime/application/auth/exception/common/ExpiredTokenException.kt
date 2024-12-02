0175ebe1bf03a102b666d757c2fad5e786225c47
saeyeonn <sy01lim@naver.com>
2024-12-02 16:58:54 +0900

    :memo: Docs: 스웨거 문서화를 위한 dto 코드 수정

--- src/main/kotlin/org/itm/ontime/application/auth/exception/common/ExpiredTokenException.kt
+++ src/main/kotlin/org/itm/ontime/application/auth/exception/common/ExpiredTokenException.kt
@@ -6,7 +6,7 @@ import org.itm.ontime.global.error.BusinessException
 @Schema(description = "Exceptions that occur when the token has expired")
 class ExpiredTokenException(
     @Schema(description = "Token that has expired", example = "eyJhbGciOiJIUzI1...")
-    private val token: String
+    val token: String
 ) : BusinessException(
     AuthErrorCode.EXPIRED_TOKEN,
     "Expired token: ${token.take(10)}"
--- src/main/kotlin/org/itm/ontime/application/auth/exception/common/InvalidRefreshTokenException.kt
+++ src/main/kotlin/org/itm/ontime/application/auth/exception/common/InvalidRefreshTokenException.kt
@@ -6,7 +6,7 @@ import org.itm.ontime.global.error.BusinessException
 @Schema(description = "Exceptions that occur when the token is invalid")
 class InvalidRefreshTokenException(
     @Schema(description = "Token that caused the error", example = "eyJhbGciOiJIUzI1...")
-    private val token: String
+    val token: String
 ) : BusinessException(
     AuthErrorCode.INVALID_REFRESH_TOKEN,
     "Invalid token: ${token.take(10)}"
