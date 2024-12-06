1ca9f5b457df27f32e1d4a9f916491883bb17c0c
saeyeonn <sy01lim@naver.com>
2024-12-07 04:35:07 +0900

    :art: Chore: 패키지 구조 변경

--- src/main/kotlin/org/itm/ontime/application/auth/exception/local/InvalidPasswordException.kt
+++
@@ -1,8 +0,0 @@
-package org.itm.ontime.application.auth.exception.local
-
-import io.swagger.v3.oas.annotations.media.Schema
-import org.itm.ontime.application.auth.exception.common.AuthErrorCode
-import org.itm.ontime.global.error.BusinessException
-
-@Schema(description = "Exceptions that occur when the password is invalid")
-class InvalidPasswordException() : BusinessException(AuthErrorCode.INVALID_PASSWORD)
