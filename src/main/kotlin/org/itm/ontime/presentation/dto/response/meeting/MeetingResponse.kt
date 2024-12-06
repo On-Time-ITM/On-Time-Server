47a7893dc8e20cc89cea1da3b8f289aea9d3cfc5
saeyeonn <sy01lim@naver.com>
2024-12-07 04:36:36 +0900

    :sparkles: Feat: 미팅 응답값에 사용자 위치 추가

--- src/main/kotlin/org/itm/ontime/presentation/dto/response/meeting/MeetingResponse.kt
+++ src/main/kotlin/org/itm/ontime/presentation/dto/response/meeting/MeetingResponse.kt
@@ -1,10 +1,9 @@
-package org.itm.ontime.presentation.meeting.response.meeting
+package org.itm.ontime.presentation.dto.response.meeting
 
 import io.swagger.v3.oas.annotations.media.Schema
-import jakarta.validation.constraints.NotBlank
-import org.itm.ontime.domain.meeting.entity.meeting.Meeting
-import org.itm.ontime.domain.payment.entity.AccountInfo
-import org.itm.ontime.presentation.meeting.response.profileImage.ProfileImageResponse
+import org.itm.ontime.domain.common.AccountInfo
+import org.itm.ontime.domain.common.Location
+import org.itm.ontime.domain.meeting.Meeting
 import java.math.BigDecimal
 import java.time.LocalDateTime
 import java.util.*
@@ -15,44 +14,43 @@ data class MeetingResponse(
         description = "Meeting ID",
         example = "123e4567-e89b-12d3-a456-426614174000"
     )
-    @field:NotBlank
     val id: UUID,
 
     @Schema(
         description = "Meeting name",
         example = "Weekly Team Meeting"
     )
-    @field:NotBlank
     val name: String,
 
     @Schema(
         description = "Meeting date and time",
         example = "2024-12-25T15:00:00"
     )
-    @field:NotBlank
     val meetingDateTime: LocalDateTime,
 
-//    @Schema(description = "Meeting location")
-//    @field:NotBlank
-//    val location: Location,
+    @Schema(
+        description = "Meeting location",
+        example = "[{\"location\": {\"latitude\": 37.7749, \"longitude\": -122.4194}, \"address\": Frontier}]"
+    )
+    val location: Location,
 
     @Schema(
         description = "Late fee amount",
         example = "5000"
     )
-    @field:NotBlank
     val lateFee: BigDecimal,
 
     @Schema(
         description = "Bank account information",
         example = "{\"accountNumber\": \"1234567890\", \"bankName\": \"Bank of America\"}"
     )
-    @field:NotBlank
     val accountInfo: AccountInfo,
 
-    // TODO : attendance
-
-    val profileImage: ProfileImageResponse
+    @Schema(
+        description = "Profile image",
+        example = "kewjfpqofjofjksldjkdjflwekflwejflqwjfek...."
+    )
+    val profileImage: String
 
 ) {
     companion object {
@@ -61,10 +59,10 @@ data class MeetingResponse(
             meeting.id,
             meeting.name,
             meeting.meetingDateTime,
-//            meeting.location,
+            meeting.location,
             meeting.lateFee,
             meeting.accountInfo,
-            ProfileImageResponse.of(meeting.profileImage)
+            meeting.profileImage
         )
 
         fun of(meetings: List<Meeting>): List<MeetingResponse> =
