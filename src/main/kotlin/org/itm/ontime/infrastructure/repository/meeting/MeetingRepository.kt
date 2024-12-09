54bcad8a831d7503b19d458dad4d9f8ae41248a5
saeyeonn <sy01lim@naver.com>
2024-12-09 17:24:01 +0900

    :bug: Fix: 미팅 시간 추적을 위한 미팅 검색 쿼리 수정

--- src/main/kotlin/org/itm/ontime/infrastructure/repository/meeting/MeetingRepository.kt
+++ src/main/kotlin/org/itm/ontime/infrastructure/repository/meeting/MeetingRepository.kt
@@ -10,12 +10,14 @@ import java.util.*
 interface MeetingRepository : JpaRepository<Meeting, UUID> {
     fun findAllByParticipantsParticipantId(participantId: UUID) : List<Meeting>
 
-    @Query("SELECT m FROM Meeting m WHERE " +
-            "m.meetingDateTime > :now AND (" +
-            "FUNCTION('date_part', 'minute', m.meetingDateTime - :now) = 10 OR " +
-            "FUNCTION('date_part', 'minute', m.meetingDateTime - :now) = 30 OR " +
-            "FUNCTION('date_part', 'minute', m.meetingDateTime - :now) = 60)"
-    )
+    @Query("""
+        SELECT m FROM Meeting m WHERE 
+        m.meetingDateTime > :now AND (
+            FUNCTION('TIMESTAMPDIFF', MINUTE, :now, m.meetingDateTime) = 10 OR 
+            FUNCTION('TIMESTAMPDIFF', MINUTE, :now, m.meetingDateTime) = 30 OR 
+            FUNCTION('TIMESTAMPDIFF', MINUTE, :now, m.meetingDateTime) = 60
+        )
+    """)
     fun findMeetingsForNotification(now: LocalDateTime?): List<Meeting>
 
 }
