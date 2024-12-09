37ecd871bb93b63e50a9c668d7bb4599faf48a7f
saeyeonn <sy01lim@naver.com>
2024-12-09 17:34:35 +0900

    :memo: Feat: 스웨거 문서화를 위한 어노테이션 추가

--- src/main/kotlin/org/itm/ontime/infrastructure/external/fcm/FCMController.kt
+++ src/main/kotlin/org/itm/ontime/infrastructure/external/fcm/FCMController.kt
@@ -1,5 +1,9 @@
 package org.itm.ontime.infrastructure.external.fcm
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
 import org.itm.ontime.infrastructure.external.fcm.dto.FCMTokenRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -9,11 +13,26 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/messages")
+@Tag(name = "FCM", description = "Firebase Cloud Messaging (FCM) APIs")
 class FCMController(
     private var fcmClient: FCMClient
 ) {
+    @Operation(
+        description = "Save token for Firebase Cloud Messaging (FCM)",
+        summary = "Save token for FCM",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Token saved successfully"
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "Invalid token format"
+            )
+        ]
+    )
     @PostMapping("/token")
-    fun saveToken(@RequestBody request: FCMTokenRequest) : ResponseEntity<Unit> {
+    fun saveToken(@RequestBody @Valid request: FCMTokenRequest) : ResponseEntity<Unit> {
         fcmClient.saveToken(request)
         return ResponseEntity.ok().build()
     }
--- src/main/kotlin/org/itm/ontime/infrastructure/external/fcm/dto/FCMTokenRequest.kt
+++ src/main/kotlin/org/itm/ontime/infrastructure/external/fcm/dto/FCMTokenRequest.kt
@@ -1,8 +1,15 @@
 package org.itm.ontime.infrastructure.external.fcm.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.NotBlank
 import java.util.*
 
+@Schema(description = "FCM token information")
 data class FCMTokenRequest(
+    @Schema(description = "User ID", example = "123-234j3-2324")
+    @field:NotBlank
     val userId: UUID,
+    @Schema(description = "Token", example = "kfjsdf20f2sldfk23krjsl")
+    @field:NotBlank
     val token: String,
 )
